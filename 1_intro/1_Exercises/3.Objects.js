9923cea26835943da89d92c73003c33ce378fab1
HolmQ84 <badboy2287>
2020-09-02 10:09:09 +0200

    Blah

--- 1_intro/1_Exercises/3.Objects.js
+++ 1_intro/1_Exercises/3.Objects.js
@@ -7,11 +7,19 @@ const myObj = {"message": "Hello, earthling! I bring peace."};
 
 // Log the message
 
+console.log(myObj.message);
+
 // --------------------------------------
 // Exercise 2 - Defining an object.
 
 // Create an object that has your name and age.
 
+const info = {
+    name: "Martin",
+    age: 36
+}
+
+console.log("Name: "+info.name+", Age: "+info.age);
 
 // --------------------------------------
 // Exercise 3 - Add a property
@@ -20,12 +28,36 @@ const stackOverflow = {};
 
 // make a rule called isAllowed and let the value be true
 
+stackOverflow.isAllowed = true;
+console.log(stackOverflow);
+
 // --------------------------------------
 // Exercise 4 - Remove a property
 
-const thisSong = {"description": "The best song in the world."}
+const thisSong = {
+    "description": "The best song in the world."
+};
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute."
 
+console.log("Before deleting: "+thisSong.description);
+delete thisSong.description;
+console.log("After deleting: "+thisSong.description);
+thisSong.about = "Just a tribute";
+console.log(thisSong.about);
 
 // --------------------------------------
+
+const fruits = ["apple", "banana"];
+console.log(fruits);
+
+fruits.push("pear");
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+
+console.log(fruits[0]);
