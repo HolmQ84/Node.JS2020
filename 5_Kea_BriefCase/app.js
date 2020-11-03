705a408354989aaa03cfd043f0670d1c9527d137
Shawk <martin-holmqvist@hotmail.com>
2020-11-03 20:01:49 +0100

    blah

--- 5_Kea_BriefCase/app.js
+++ 5_Kea_BriefCase/app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // The next two lines is to make sure that post works
+app.use(express.urlencoded({extended: true})) // This is so that we can use url in upload.js
 
 const fs = require('fs');
 
@@ -35,10 +35,10 @@ app.post("/form", (req, res) => {
     res.send(req.body);
 })
 
-/*
+
 const uploadRouter = require('./routes/upload.js')
 app.use(uploadRouter);
-*/
+
 
 const port = process.env.PORT || 8080;
 
@@ -47,27 +47,4 @@ app.listen(port, (error) => {
         console.log("Error starting the server :-(", error);
     }
     console.log("Server running on port", Number(port));
-});
-
-const crypto = require('crypto');
-
-const path = require('path');
-
-const uploads = [];
-
-app.get('/uploads/:id', (req, res) => {
-    const foundUpload = uploads.find(upload => upload.id === req.params.id);
-    return res.send({ data: foundUpload });
-})
-
-app.post('/uploads', (req, res) => {
-    const id = crypto.randomBytes(28).toString("hex");
-    console.log(req.body);
-    uploads.push({...req.body, id });
-    console.log(uploads)
-    return res.send({ id });
-});
-
-app.get("/uploads", (req, res) => {
-    res.sendFile(path.join(__dirname+"/../public/upload/upload.html"));
 });
