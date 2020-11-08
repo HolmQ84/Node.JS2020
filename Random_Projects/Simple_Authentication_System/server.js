c2711b805d85f92a757c856c0ab1309bfa2f8eae
Shawk <martin-holmqvist@hotmail.com>
2020-11-08 22:12:31 +0100

    blah

--- Random_Projects/Simple_Authentication_System/server.js
+++ Random_Projects/Simple_Authentication_System/server.js
@@ -6,21 +6,24 @@ app.use(express.json())
 
 const users = [];
 
+// Get all users
 app.get('/users', (req, res) => {
     res.json(users);
 })
 
+// Create User
 app.post('/users', async (req, res) => {
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
         const user = { name: req.body.name, password: hashedPassword }
-        users.push(user)
-        res.status(201).send()
+        users.push(user);
+        res.status(201).send();
     } catch {
-        res.status(500).send()
+        res.status(500).send();
     }
 })
 
+// User login
 app.post('/users/login', async (req, res) => {
     const user = users.find( user => user.name = req.body.name )
     if (user == null) {
@@ -37,5 +40,11 @@ app.post('/users/login', async (req, res) => {
     }
 })
 
-app.listen(3000);
+const port = process.env.PORT || 8080;
 
+app.listen(port, (error) => {
+    if (error) {
+        console.log("Fejl ved opstart af server.");
+    }
+    console.log("Serveren kører på port: ", port);
+});
